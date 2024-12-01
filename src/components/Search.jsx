7fed7921752b059bac9eb6a15aab4c608709bdf2
Korney Chervonenko <pokryshkinkorney@gmail.com>
2024-12-01 18:22:20 +0200

    Search placeholder

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,7 +1,17 @@
+import './Search.scss';
+
 export default function Search() {
 	return (
-		<form>
-			<legend>Search panel</legend>
+		<form className="search">
+			<input type="text" placeholder="🔎 Search for a country..." required />
+			<select name="region">
+				<option value="all">Filter by region</option>
+				<option value="africa">Africa</option>
+				<option value="america">America</option>
+				<option value="asia">Asia</option>
+				<option value="europe">Europe</option>
+				<option value="oceania">Oceania</option>
+			</select>
 		</form>
 	);
 }
