00a50d5ffc2768dfb4844d047c94a4630bfcce75
Korney Chervonenko <pokryshkinkorney@gmail.com>
2025-02-10 13:21:12 +0200

    virtualization for CountriesList

--- src/components/CountriesList.jsx
+++ src/components/CountriesList.jsx
@@ -1,9 +1,11 @@
+import { useState } from 'react';
 import CountryCard from './CountryCard.jsx';
-
-import './CountriesList.scss';
 import { useCountriesContext } from '../contexts/CountriesContext.jsx';
+import './CountriesList.scss';
 
 export default function CountriesList() {
+	const maxVisibleItems = 10;
+	const [range, setRange] = useState({ start: 0, end: maxVisibleItems });
 	const { countries, region, searchQuery } = useCountriesContext();
 	const countriesFilteredByRegion =
 		region === 'All' ? [...countries] : countries.filter((country) => country.region === region);
@@ -14,11 +16,30 @@ export default function CountriesList() {
 					country.name.toLowerCase().startsWith(searchQuery)
 			  );
 
+	const visibleCountries = filteredCountries.slice(range.start, range.end);
+
+	function handleScroll(e) {
+		// console.log(e);
+		// console.log(e.deltaY);
+		// console.log(range, visibleCountries.length);
+		// e.preventDefault();
+		if (e.nativeEvent.wheelDeltaY < 0 && range.end < filteredCountries.length)
+			setRange((currRange) => ({
+				start: Math.min(filteredCountries.length - maxVisibleItems, currRange.start + 1),
+				end: Math.min(filteredCountries.length, currRange.end + 1),
+			}));
+		if (e.nativeEvent.wheelDeltaY > 0 && range.start > 0)
+			setRange((currRange) => ({
+				start: Math.max(0, currRange.start - 1),
+				end: Math.max(maxVisibleItems, currRange.end - 1),
+			}));
+	}
+
 	return (
 		<>
 			<h2 className="visually-hidden">countries list</h2>
-			<ul className="countries-list">
-				{filteredCountries.map((country, index) => (
+			<ul className="countries-list" onWheel={handleScroll}>
+				{visibleCountries.map((country, index) => (
 					<CountryCard country={country} key={country.name} />
 				))}
 			</ul>
